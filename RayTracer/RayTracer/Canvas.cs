99a3c4584048448b54600f3f0d99c6cb8e474a2d
xtozero <xtozero@naver.com>
2019-07-24 20:20:55 +0900

    Add Canvas

---
+++ RayTracer/RayTracer/Canvas.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+
+namespace RayTracer
+{
+    public class Canvas
+    {
+        public Tuple PixelAt(int x, int y)
+        {
+            return _pixels[y, x];
+        }
+
+        public void WritePixel(int x, int y, Tuple color)
+        {
+            _pixels[y, x] = color;
+        }
+
+        public string ToPPM()
+        {
+            StringWriter ppm = new StringWriter();
+
+            ppm.WriteLine("P3");
+            ppm.WriteLine("{0} {1}", Width, Height);
+            ppm.WriteLine("255");
+            int writedChar = 1;
+            for (int y = 0; y < Height; ++y)
+            {
+                ppm.Write(" ");
+                for (int x = 0; x < Width; ++x)
+                {
+                    Tuple c = _pixels[y, x];
+                    byte[] colors = new byte[]
+                    {
+                        (byte)System.Math.Clamp(System.Math.Round(c.R * 255), 0, 255),
+                        (byte)System.Math.Clamp(System.Math.Round(c.G * 255), 0, 255),
+                        (byte)System.Math.Clamp(System.Math.Round(c.B * 255), 0, 255)
+                    };
+
+                    foreach (byte color in colors)
+                    {
+                        if ( writedChar + ( color.ToString().Length + 1 ) > 70 )
+                        {
+                            writedChar = 1;
+                            ppm.WriteLine();
+                            ppm.Write(" ");
+                        }
+
+                        ppm.Write("{0} ", color);
+                        writedChar += (color.ToString().Length + 1);
+                    }
+                }
+                writedChar = 1;
+                ppm.WriteLine();
+            }
+
+            return ppm.ToString();
+        }
+
+        public Canvas(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            _pixels = new Tuple[Height, Width];
+
+            for ( int y = 0; y < Height; ++y )
+            {
+                for ( int x = 0; x < Width; ++x )
+                {
+                    _pixels[y, x] = Tuple.Color(0, 0, 0);
+                }
+            }
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        Tuple[,] _pixels;
+    }
+}
---
+++ RayTracer/UnitTest/CanvasTest.cs
@@ -0,0 +1,89 @@
+﻿using RayTracer;
+using Xunit;
+
+namespace UnitTest
+{
+    public class CanvasTest
+    {
+        [Fact]
+        public void TestCase1()
+        {
+            Canvas c = new Canvas(10, 20);
+            Assert.Equal(10, c.Width);
+            Assert.Equal(20, c.Height);
+
+            Tuple white = Tuple.Color(0, 0, 0);
+            for ( int y = 0; y < c.Height; ++y )
+            {
+                for ( int x = 0; x < c.Width; ++x )
+                {
+                    Assert.Equal(white, c.PixelAt(x, y));
+                }
+            }
+        }
+
+        [Fact]
+        public void TestCase2()
+        {
+            Canvas c = new Canvas(10, 20);
+            Tuple red = Tuple.Color(1, 0, 0);
+            c.WritePixel(2, 3, red);
+            Assert.Equal(red, c.PixelAt(2, 3));
+        }
+
+        [Fact]
+        public void TestCase3()
+        {
+            Canvas c = new Canvas(5, 3);
+            string ppm = c.ToPPM();
+            Assert.StartsWith("P3\r\n5 3\r\n255", ppm);
+        }
+
+        [Fact]
+        public void TestCase4()
+        {
+            Canvas c = new Canvas(5, 3);
+            Tuple c1 = Tuple.Color(1.5f, 0, 0);
+            Tuple c2 = Tuple.Color(0, 0.5f, 0);
+            Tuple c3 = Tuple.Color(-0.5f, 0, 1);
+            c.WritePixel(0, 0, c1);
+            c.WritePixel(2, 1, c2);
+            c.WritePixel(4, 2, c3);
+            string ppm = c.ToPPM();
+            Assert.Contains(
+                " 255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \r\n" +
+                " 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 \r\n" +
+                " 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 \r\n"
+                , ppm);
+        }
+
+        [Fact]
+        public void TestCase5()
+        {
+            Canvas c = new Canvas(10, 2);
+            Tuple PeachOrange = Tuple.Color(1, 0.8f, 0.6f);
+            for (int y = 0; y < c.Height; ++y)
+            {
+                for (int x = 0; x < c.Width; ++x)
+                {
+                    c.WritePixel(x, y, PeachOrange);
+                }
+            }
+            string ppm = c.ToPPM();
+            Assert.Contains(
+                " 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 \r\n" +
+                " 153 255 204 153 255 204 153 255 204 153 255 204 153 \r\n" +
+                " 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 \r\n" +
+                " 153 255 204 153 255 204 153 255 204 153 255 204 153 \r\n"
+                , ppm);
+        }
+
+        [Fact]
+        public void TestCase6()
+        {
+            Canvas c = new Canvas(5, 3);
+            string ppm = c.ToPPM();
+            Assert.EndsWith("\n", ppm);
+        }
+    }
+}
