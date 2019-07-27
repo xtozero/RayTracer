973ce828608203224736f8873c69c29a29b1a7f3
xtozero <xtozero@naver.com>
2019-07-27 17:15:49 +0900

    Clock Hour Test Scene

---
+++ RayTracer/UnitTest/ClockHourTest.cs
@@ -0,0 +1,35 @@
+﻿using RayTracer;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ClockHourTest
+    {
+        [Fact]
+        public void Simulate()
+        {
+            Canvas c = new Canvas(256, 256);
+
+            float halfHeight = c.Height * 0.5f;
+            Tuple p = Tuple.Point(0, halfHeight * ( 3.0f / 4.0f ), 0);
+
+            Matrix t = Transformation.RotationZ(System.MathF.PI / 6);
+
+            float halfWidth = c.Width * 0.5f;
+            Tuple white = Tuple.Color(1, 1, 1);
+            for ( int i = 0; i < 12; ++i )
+            {
+                c.WritePixel((int)System.MathF.Round(p.X + halfWidth), (int)System.MathF.Round(p.Y + halfHeight), white);
+                p = t * p;
+            }
+
+            using (System.IO.StreamWriter sw = 
+                new System.IO.StreamWriter(@"./clockHour.ppm"))
+            {
+                sw.Write(c.ToPPM());
+            }
+
+            Assert.True(true);
+        }
+    }
+}
