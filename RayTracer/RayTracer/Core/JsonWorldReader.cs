ec2adf18c5c6138da11e36cd5b432a8020e2f1bd
xtozero <xtozero@naver.com>
2019-10-17 21:23:07 +0900

    Add Json.Net module

---
+++ RayTracer/RayTracer/Core/JsonWorldReader.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace RayTracer
+{
+    public static class JsonWorldReader
+    {
+        public static World BuildWolrd(string filePath)
+        {
+            JObject worldAsset = new JObject();
+
+            World w = new World();
+            return w;
+        }
+    }
+}
